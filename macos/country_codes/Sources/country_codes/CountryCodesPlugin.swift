e6e58187401231802b31ccbfe6d90afa9b686b64
Yuri Komlev <Yuri.Komlev@itechart-group.com>
2025-07-13 19:52:55 +0530

    Fix deprecation warnings noise

--- macos/country_codes/Sources/country_codes/CountryCodesPlugin.swift
+++ macos/country_codes/Sources/country_codes/CountryCodesPlugin.swift
@@ -1,3 +1,4 @@
+
 import Cocoa
 import FlutterMacOS
 
@@ -11,26 +12,46 @@ public class CountryCodesPlugin: NSObject, FlutterPlugin {
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
     case "getRegion":
+      if #available(macOS 13, *) {
+        result(Locale.current.region.identifier)
+      } else {
         result(Locale.current.regionCode)
-        break
+      }
+      break
     case "getLanguage":
+      if #available(macOS 13, *) {
+        result(Locale.current.language.languageCode.identifier)
+      } else {
         result(Locale.current.languageCode)
-        break
+      }
+      break
     case "getLocale":
-        result([Locale.current.languageCode as Any, Locale.current.regionCode as Any, getLocalizedCountryNames(localeTag: call.arguments as? String)])
-        break
+      if #available(macOS 13, *) {
+        result([
+          Locale.current.language.languageCode.identifier as Any,
+          Locale.current.region.identifier as Any,
+          getLocalizedCountryNames(localeTag: call.arguments as? String)
+        ])
+      } else {
+        result([
+          Locale.current.languageCode as Any,
+          Locale.current.regionCode as Any,
+          getLocalizedCountryNames(localeTag: call.arguments as? String)
+        ])
+      }
+      break
     default:
-        result(FlutterMethodNotImplemented);
+      result(FlutterMethodNotImplemented)
     }
   }
-  
-  func getLocalizedCountryNames(localeTag: String?) -> Dictionary<String,String> {
-      var localizedCountries:Dictionary<String,String> = [String: String]()
-  
-      for countryCode in NSLocale.isoCountryCodes {
-          let countryName: String? = NSLocale(localeIdentifier: localeTag ?? Locale.preferredLanguages[0]).displayName(forKey: .countryCode, value: countryCode)
-          localizedCountries[countryCode.uppercased()] = countryName ?? "";
-      }
-      return localizedCountries
+
+  func getLocalizedCountryNames(localeTag: String?) -> Dictionary<String, String> {
+    var localizedCountries: [String: String] = [:]
+    for countryCode in NSLocale.isoCountryCodes {
+      let locale = NSLocale(localeIdentifier: localeTag ?? Locale.preferredLanguages[0])
+      let countryName = locale.displayName(forKey: .countryCode, value: countryCode)
+      localizedCountries[countryCode.uppercased()] = countryName ?? ""
+    }
+    return localizedCountries
   }
 }
